8a430292ab5c017e7fbc405dbeb44e3a7d0e32df
Lucas Cid <lucasfelipaaa@gmail.com>
2025-05-20 23:16:45 -0300

    feat: adiciona fade para controlar close de dropdown

--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -1,17 +1,22 @@
 import { useDropdow } from "@/store/dropdown";
 import { useColorScheme } from "nativewind";
 import { ReactNode } from "react";
-import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import { TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export function ScreenWrapper({ children, className }: { children: ReactNode, className?: string }) {
   const { colorScheme } = useColorScheme();
+  const { isOpen, setIsOpen } = useDropdow();  
   return (
     <SafeAreaView
-      className={"bg-zinc-100 dark:bg-zinc-950 h-full w-full px-5 m-0 " + className}
+      className={"bg-zinc-100 dark:bg-zinc-950 h-full w-full px-4 " + className}
       style={{ backgroundColor: colorScheme === "dark" ? "#09090b" : "#f4f4f5" }}
       edges={["top"]}
     >
+      {isOpen && (
+        <TouchableOpacity onPress={() => setIsOpen(false)} className="absolute inset-0 z-[10000] bg-zinc-950/80">
+        </TouchableOpacity>
+      )}
       {children}
     </SafeAreaView>
   );
